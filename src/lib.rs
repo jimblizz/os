99fbd7129d3df8bcaeb8f8d15fc0d3a7ff7126f4
James Blizzard <james.blizzard@browserlondon.com>
2016-10-22 16:15:19 +0100

    Memory areas an panic impl

--- src/lib.rs
+++ src/lib.rs
@@ -23,22 +23,29 @@ extern fn eh_personality() {
 }
 
 #[lang = "panic_fmt"]
-extern fn rust_begin_panic() -> ! {
-    loop {}
+extern fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop{}
 }
 
 #[no_mangle]
 pub extern fn kmain(multiboot_information_address: usize) -> ! {
 
     // ATTENTION: we have a very small stack and no guard page
-//    vga_buffer::clear_screen();   // This doesn't work for some reason?
+    vga_buffer::clear_screen();   // This doesn't work for some reason?
     println!("Starting...");
 
-//    let boot_info = unsafe{ mulitboot2::load(multiboot_information_address) };
-//    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
+    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
 
-
+    println!("Memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
+    }
 
     println!("Ready");
+
+    panic!();
     loop { }
 }

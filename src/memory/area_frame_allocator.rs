214cccac690b537e059ea90d1721a821b7ca535c
James Blizzard <james.blizzard@browserlondon.com>
2016-10-23 12:11:29 +0100

    A paging system that I don't really understand... moving on

--- src/memory/area_frame_allocator.rs
+++ src/memory/area_frame_allocator.rs
@@ -1,7 +1,11 @@
-use memory::Frame;
-use memory::FrameAllocator;
+use memory::{Frame, FrameAllocator};
 use multiboot2::{MemoryAreaIter, MemoryArea};
 
+/// A frame allocator that uses the memory areas from the multiboot information structure as
+/// source. The {kernel, multiboot}_{start, end} fields are used to avoid returning memory that is
+/// already in use.
+///
+/// `kernel_end` and `multiboot_end` are _inclusive_ bounds.
 pub struct AreaFrameAllocator {
     next_free_frame: Frame,
     current_area: Option<&'static MemoryArea>,
@@ -13,24 +17,12 @@ pub struct AreaFrameAllocator {
 }
 
 impl AreaFrameAllocator {
-    fn choose_next_area(&mut self) {
-        self.current_area = self.areas.clone().filter(|area| {
-            let address = area.base_addr + area.length - 1;
-            Frame::containing_address(address as usize) >= self.next_free_frame
-        }).min_by_key(|area| area.base_addr);
-
-        if let Some(area) = self.current_area {
-            let start_frame = Frame::containing_address(area.base_addr as usize);
-            if self.next_free_frame < start_frame {
-                self.next_free_frame = start_frame;
-            }
-        }
-    }
-
-    pub fn new(kernel_start: usize, kernel_end: usize,
-               multiboot_start: usize, multiboot_end: usize,
-               memory_areas: MemoryAreaIter) -> AreaFrameAllocator
-    {
+    pub fn new(kernel_start: usize,
+               kernel_end: usize,
+               multiboot_start: usize,
+               multiboot_end: usize,
+               memory_areas: MemoryAreaIter)
+               -> AreaFrameAllocator {
         let mut allocator = AreaFrameAllocator {
             next_free_frame: Frame::containing_address(0),
             current_area: None,
@@ -43,14 +35,31 @@ impl AreaFrameAllocator {
         allocator.choose_next_area();
         allocator
     }
+
+    fn choose_next_area(&mut self) {
+        self.current_area = self.areas
+            .clone()
+            .filter(|area| {
+                let address = area.base_addr + area.length - 1;
+                Frame::containing_address(address as usize) >= self.next_free_frame
+            })
+            .min_by_key(|area| area.base_addr);
+
+        if let Some(area) = self.current_area {
+            let start_frame = Frame::containing_address(area.base_addr as usize);
+            if self.next_free_frame < start_frame {
+                self.next_free_frame = start_frame;
+            }
+        }
+    }
 }
 
 impl FrameAllocator for AreaFrameAllocator {
     fn allocate_frame(&mut self) -> Option<Frame> {
         if let Some(area) = self.current_area {
-            // "Clone" the frame to return it if it's free. Frame doesn't
+            // "clone" the frame to return it if it's free. Frame doesn't
             // implement Clone, but we can construct an identical frame.
-            let frame = Frame{ number: self.next_free_frame.number };
+            let frame = Frame { number: self.next_free_frame.number };
 
             // the last frame of the current area
             let current_area_last_frame = {
@@ -63,14 +72,10 @@ impl FrameAllocator for AreaFrameAllocator {
                 self.choose_next_area();
             } else if frame >= self.kernel_start && frame <= self.kernel_end {
                 // `frame` is used by the kernel
-                self.next_free_frame = Frame {
-                    number: self.kernel_end.number + 1
-                };
+                self.next_free_frame = Frame { number: self.kernel_end.number + 1 };
             } else if frame >= self.multiboot_start && frame <= self.multiboot_end {
                 // `frame` is used by the multiboot information structure
-                self.next_free_frame = Frame {
-                    number: self.multiboot_end.number + 1
-                };
+                self.next_free_frame = Frame { number: self.multiboot_end.number + 1 };
             } else {
                 // frame is unused, increment `next_free_frame` and return it
                 self.next_free_frame.number += 1;
@@ -83,7 +88,7 @@ impl FrameAllocator for AreaFrameAllocator {
         }
     }
 
-    fn deallocate_frame(&mut self, frame: Frame) {
+    fn deallocate_frame(&mut self, _frame: Frame) {
         unimplemented!()
     }
 }
